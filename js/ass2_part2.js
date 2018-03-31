d013ed573f9e8a1e752959cf3a1015764ec434bd
Kaas <alexkaas2@gmail.com>
2018-03-31 16:04:13 +0200

    Added Bar plot

--- js/ass2_part2.js
+++ js/ass2_part2.js
@@ -2,6 +2,7 @@
 //Width and height
 var w = 1000;
 var h = 600;
+var dur = 4000;
 
 //Define map projection
 var projection = d3.geoMercator()
@@ -22,7 +23,7 @@ var map_svg = d3.select("#part2_map")
 
 var color = d3.scaleOrdinal()
               .range(["rgb(12, 150, 50)","rgb(0, 128, 128) ","rgb(0, 0, 128) ","rgb(128, 0, 0)", "rgb(200, 150, 180)"])
-
+var barPadding = 1;
 var xScale;
 var yScale;
 var xAxis;
@@ -30,7 +31,120 @@ var yAxis;
 var dots;
 var time_svg;
 var brush;
+var dataset=[
+  320
+, 292
+, 280
+, 260
+, 285
+, 198
+, 99
+, 88
+, 101
+, 83
+, 102
+, 108
+, 167
+, 144
+, 152
+, 178
+, 140
+, 207
+, 237
+, 246
+, 247
+, 309
+, 343
+, 323];
+var hours=['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23'];
+var barH = 200
+var barW = 800
+    //Create scale functions
+var xBarScale = d3.scaleBand()
+                .domain(hours.map(function(d) { return d; }))
 
+                .range([60, barW])
+                .paddingInner(0.001);
+
+var ymax = d3.max(dataset);
+
+var yBarScale = d3.scaleLinear()
+                .domain([0 , ymax])
+                .range([barH,20]);           
+
+    //Define X axis
+var xAxis = d3.axisBottom(xBarScale);
+        
+    //Define Y axis
+var yAxis = d3.axisLeft(yBarScale)
+                .tickValues(d3.range(0,ymax+1,(ymax < 5) ? 1 : ymax/5));
+
+var bar_svg = d3.select("#part2_bar")
+      .append("svg")
+      .attr("width", barW+80)
+      .attr("height", barH+40);
+
+bar_svg.selectAll("rect")
+         .data(dataset)
+         .enter()
+         .append("rect")
+         .attr("x", function(d, i) {
+            return xBarScale(i);
+         })
+         .attr("y", function(d) {
+            return yBarScale(d);
+         })
+         .attr("width", xBarScale.bandwidth()-4)
+         .attr("height", function(d) {
+            return barH - yBarScale(d)
+         })
+         .attr("fill", function(d) {
+          return "rgb(0, 0, " + Math.round(yBarScale(d)) + ")";
+         });
+bar_svg.selectAll("text")
+         .data(dataset)
+         .enter()
+         .append("text")
+         .text(function(d) {
+            return d;
+         })
+         .attr("text-anchor", "middle")
+         .attr("x", function(d, i) {
+            return xBarScale(i)+15;
+         })
+         .attr("y", function(d) {
+            return barH - yBarScale(d) + 15;
+         })
+         .attr("font-family", "sans-serif")
+         .attr("font-size", "11px")
+         .attr("fill", "white");
+
+ //Create X axis
+        bar_svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + barH + ")")
+            .call(xAxis);
+
+        //Create Y axis
+        bar_svg.append("g")
+           .attr("class", "y axis")
+           .attr("transform", "translate(60," + 0 + ")")
+           .call(yAxis.tickValues(d3.range(0,ymax+1,(ymax < 5) ? 1 : ymax/5)));    
+
+        // Create xAxis label
+        bar_svg.append("text")
+            .attr("transform", "translate(" + (barW/2) + " ," + (barH + 10 + 30) + ")")
+            .style("text-anchor", "middle")
+            .text("Hours");    
+
+        // Create yAxis label
+        bar_svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - 0)
+            .attr("x", 0 - (barH / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("# of Murders"); 
 //Load in GeoJSON data
 d3.json("data/boroughs.json", function(json) {
     color.domain(json.features.map(function(j) {
@@ -200,10 +314,12 @@ d3.json("data/boroughs.json", function(json) {
 function brushed () {
   var sel = d3.event.selection
   if (sel != null) {
+    var dataset=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
     d3.selectAll('.dot')
       .style("fill", function(d) {
         var dotDate = xScale(new Date(d.RPT_DT));
         if (sel[0] <= dotDate && dotDate <= sel[1]) {
+            dataset[d.CMPLNT_FR_TM]=dataset[d.CMPLNT_FR_TM]+1;
             return "yellow";
         } else { 
             return "transparent";
@@ -217,8 +333,41 @@ function brushed () {
             return "transparent";
         }
         });
+        ymax=d3.max(dataset);
+        yBarScale.domain([0, ymax]);
+        yAxis = d3.axisLeft(yBarScale)
+                .tickValues(d3.range(0,ymax+1,(ymax < 5) ? 1 : ymax/5));
 
-  }
+      bar_svg.selectAll("rect")
+         .data(dataset)
+         .transition()
+         .duration(dur)
+        .attr("y", function(d) {
+            return yBarScale(d);
+         })
+         .attr("height", function(d) {
+            return barH - yBarScale(d)
+         })
+         .attr("fill", function(d) {
+          return "rgb(0, 0, " + Math.round(255-yBarScale(d)) + ")";
+         });
+//Update all labels
+          bar_svg.selectAll("text")
+             .data(dataset)
+             .transition()
+             .duration(dur)
+             .text(function(d) {
+                return d;
+             })
+             .attr("y", function(d) {
+                return yBarScale(d)+15;
+             });
+     bar_svg.select(".y.axis")
+        .transition()
+        .duration(dur)
+        .call(yAxis.tickValues(d3.range(0,ymax+1,(ymax < 5) ? 1 : ymax/5)));  }
+      console.log(ymax);
+
 }
   });
 });
