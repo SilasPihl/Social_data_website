12ca93591fb5e90f3c0e6c62f978f5c48bd56e55
SilasPihl <pihl92@hotmail.com>
2018-05-09 14:35:22 +0200

    Alt i funktioner, især time og map var meget fragmenteret

--- js/final_assignment.js
+++ js/final_assignment.js
@@ -1,148 +1,25 @@
-//Width and height
-
-var chartDiv = document.getElementById("d3_map");
-var w = chartDiv.clientWidth;
-// var w = 500;
-var h = 550;
 var dur = 100;
 var bronxActive=true;
 var manhattanActive=true;
 var queensActive=true;
 var brooklynActive=true;
 var statenIslandActive=true;
-//Define map projection
-var projection = d3.geoMercator()
-        // .scale(50000)
-        .scale(90*w+1300)
-        .center([-74, 40.71])
-        .translate([w * 0.5, h * 0.5]);
 
-//Define path generator
-var path = d3.geoPath()
-             .projection(projection);
-
-var color = d3.scaleOrdinal()
+color = d3.scaleOrdinal()
               .range(["rgb(12, 150, 50)","rgb(0, 128, 128) ","rgb(0, 0, 128) ","rgb(128, 0, 0)", "rgb(200, 150, 180)"])
 
-var barPadding = 1;
-
-//Create SVG element
-map_svg = d3.select("#d3_map")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h)
-            .append("g");
-
-
-
-
-var dataset_all= [320, 292, 280, 260, 285, 198, 99, 88, 101, 83, 102, 108, 167, 144, 152, 178, 140, 207, 237, 246, 247, 309, 343, 323];
-var dataset = dataset_all;
+dataset_all= [320, 292, 280, 260, 285, 198, 99, 88, 101, 83, 102, 108, 167, 144, 152, 178, 140, 207, 237, 246, 247, 309, 343, 323];
+dataset = dataset_all;
 // var hours=['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23'];
-var hours = _.range(24);
-
-var chartDiv = document.getElementById("d3_bar");
-var barW = chartDiv.clientWidth;
-var barH = 200;
-
-// Margins
-var bar_m = {
-    t: 10,
-    r: 40,
-    b: 30,
-    l: 40
-}
+hours = _.range(24);
 
 sel_map = [[0,0], [100000,100000]];
 sel_time = [0,1000000];
 sel_bar = [0,1000000];
+fromBar = false;
 
-//Create scale functions
-xBarScale = d3.scaleBand()
-              .domain(hours.map(function(d) { return d; }))
-              .range([60, barW])
-              .paddingInner(0.001);
-
-var ymax = d3.max(dataset);
-
-yBarScale = d3.scaleLinear()
-              .domain([0 , ymax])
-              .range([barH,20]);           
-
-//Define X axis
-xAxis_bar = d3.axisBottom(xBarScale);
-        
-//Define Y axis
-yAxis_bar = d3.axisLeft(yBarScale)
-          .tickValues(d3.range(0,ymax+1,(ymax < 5) ? 1 : ymax * 0.2));
-
-bar_svg = d3.select("#d3_bar")
-            .append("svg")
-            .attr("width", barW + bar_m.l + bar_m.r)
-            .attr("height", barH + bar_m.t + bar_m.b);
-
-bar_svg.selectAll("rect")
-       .data(dataset)
-       .enter()
-       .append("rect")
-       .attr("x", function(d, i) {
-          return xBarScale(i);
-       })
-       .attr("y", function(d) {
-          return yBarScale(d);
-       })
-       .attr("width", xBarScale.bandwidth()-4)
-       .attr("height", function(d) {
-          return barH - yBarScale(d)
-       })
-       .attr("fill", function(d) {
-        return "rgb(0, 0, " + Math.round(yBarScale(d)) + ")";
-       });
-
-bar_svg.selectAll("text")
-       .data(dataset)
-       .enter()
-       .append("text")
-       .text(function(d) {
-          return d;
-       })
-       .attr("text-anchor", "middle")
-       .attr("x", function(d, i) {
-          return xBarScale(i)+w/50;
-       })
-       .attr("y", function(d) {
-          return barH - yBarScale(d) - 20;
-       })
-       .attr("font-family", "sans-serif")
-       .attr("font-size", "10px")
-       .attr("fill", "white");
-
-//Create X axis
-bar_svg.append("g")
-       .attr("class", "x axis")
-       .attr("transform", "translate(0," + barH + ")")
-       .call(xAxis_bar);
-
-//Create Y axis
-bar_svg.append("g")
-       .attr("class", "y axis")
-       .attr("transform", "translate(60," + 0 + ")")
-       .call(yAxis_bar.tickValues(d3.range(0,ymax+1,(ymax < 5) ? 1 : ymax * 0.2)));;    
-
-// Create xAxis label
-bar_svg.append("text")
-       .attr("transform", "translate(" + (barW * 0.5 + bar_m.l) + " ," + (barH + bar_m.t + bar_m.b) + ")")
-       .style("text-anchor", "middle")
-       .text("Hours");    
-
-// Create yAxis label
-bar_svg.append("text")
-       .attr("transform", "rotate(-90)")
-       .attr("y", 0 - 0)
-       .attr("x", 0 - (barH * 0.5))
-       .attr("dy", "1em")
-       .style("text-anchor", "middle")
-       .text("No of accidents"); 
+formatYear = d3.timeFormat("%b-%y");
+date_format = d3.timeFormat("%Y-%m-%d");
 
 //Load in GeoJSON data
 d3.json("data/boroughs.json", function(json) {
@@ -150,91 +27,55 @@ d3.json("data/boroughs.json", function(json) {
       return j.properties.BoroCode;
   }));
 
-  //Bind data and create one path per GeoJSON feature
-
-  json_fet = json.features
-  map_svg.selectAll("path")
-         .data(json.features)
-         .enter()
-         .append("path")
-         .attr("d", path)
-         .style("fill", function (d) {
-          return color(d.properties.BoroCode)
-         });
-
-
-//Bronx
-//Manhattan
-//Queens
-//StatenIsland
-//Brooklyn
+  //Bronx
+  //Manhattan
+  //Queens
+  //StatenIsland
+  //Brooklyn
   d3.csv("data/all1718Bronx.csv", function (bronx) {
     d3.csv("data/all1718Manhattan.csv", function (manhattan) {
       d3.csv("data/all1718Queens.csv", function (queens) {
         d3.csv("data/all1718StatenIsland.csv", function (statenIsland) {
           d3.csv("data/all1718Brooklyn.csv", function (brooklyn) {
-  data = bronx.concat(manhattan).concat(queens).concat(statenIsland).concat(brooklyn);  
-  console.log("data: + "+ data.length + " bronx: "+ bronx.length+ " manhattan: "+ manhattan.length);  
-  date_format = d3.timeFormat("%Y-%m-%d");
-  data.forEach (function(d) {
-    //console.log(d.ON_STREET_NAME);
-    d.DATE = date_format(new Date(d.DATE));
-    d.TIME = d.TIME.split(":")[0];
-  }); 
+            data = bronx.concat(manhattan).concat(queens).concat(statenIsland).concat(brooklyn);  
+            console.log("data: + "+ data.length + " bronx: "+ bronx.length+ " manhattan: "+ manhattan.length);  
+            data.forEach (function(d) {
+              //console.log(d.ON_STREET_NAME);
+              d.DATE = date_format(new Date(d.DATE));
+              d.TIME = d.TIME.split(":")[0];
+            }); 
 
-  data = data.filter (function(d, i) {
-    if (i%4 != 0) {
-      return false;
-    }
-    else {
-      return true;
-    }
+            data = data.filter (function(d, i) {
+              if (i%20 != 0) {
+                return false;
+              }
+              else {
+                return true;
+              }
+            });
+
+            init_all(data,json)
+
+          });
+        });
+      });
+    });
   });
-  dots = map_svg.selectAll("circle")
-                .data(data)
-                .enter()
-                .append("circle")
-                .attr("class", "dot activeDot")
-                .attr("cx", function(d) {
-                    return projection([d.LONGITUDE, d.LATITUDE])[0];
-                })
-                .attr("cy", function(d) {
-                    return projection([d.LONGITUDE, d.LATITUDE])[1];
-                })
-                .attr("r", 2)
-                .on("mouseover", function(d) {
-                  if(d3.select(this).attr("class")=="dot activeDot"){
-                  console.log(""+d.UNIQUE_KEY);
-                  }
-                //})
-               // .append("title")         //Simple tooltip
-                //.text(function(d) {
-                  //   return "Street Name "+d.ON_STREET_NAME+ " key " + d.UNIQUE_KEY;
-                });
+});
 
+function init_all(data,json) {
 
- /* map_svg.selectAll("text")
-         .data(json_fet)
-         .enter()
-         .append("text")
-         .attr("fill", "black")
-         .attr("class", "map-label")
-         .attr("transform", function(d) {
-             var c = path.centroid(d);
-             return "translate("+c[0]+","+c[1]+")"
-         })
-         .attr("text-anchor", "middle")
-         .text(function(d) {
-             return d.properties.BoroName;
-         });*/
+  initMapChart(data, json)
 
   // Counting Accidents per day
   accidentsPerDay = parseData(data);
 
   updatedKeyValueArray = accidentsPerDay;
-  createLineChart(accidentsPerDay)
-  });
-});});});});});
+  
+  initLineChart(accidentsPerDay)
+
+  initBarChart();
+}
 
 function parseData(data) {
   // Counting murders per day
@@ -265,70 +106,122 @@ function fillWithNullDays (data) {
   return data;
 }
 
+function initMapChart (data, json) {
+  var chartDiv = document.getElementById("d3_map");
+  map_w = chartDiv.clientWidth;
+  map_h = 550;
 
-function createLineChart (data) {
+  //Define map projection
+  var projection = d3.geoMercator()
+          // .scale(50000)
+          .scale(90*map_w+1300)
+          .center([-74, 40.71])
+          .translate([map_w * 0.5, map_h * 0.5]);
+
+  //Define path generator
+  var path = d3.geoPath()
+               .projection(projection);
+
+  //Bind data and create one path per GeoJSON feature
+  //Create SVG element
+  map_svg = d3.select("#d3_map")
+              .append("svg")
+              .attr("width", map_w)
+              .attr("height", map_h)
+              .append("g");
+
+  json_fet = json.features
+  map_svg.selectAll("path")
+         .data(json.features)
+         .enter()
+         .append("path")
+         .attr("d", path)
+         .style("fill", function (d) {
+          return color(d.properties.BoroCode)
+         });
+
+  dots = map_svg.selectAll("circle")
+                .data(data)
+                .enter()
+                .append("circle")
+                .attr("class", "dot activeDot")
+                .attr("cx", function(d) {
+                    return projection([d.LONGITUDE, d.LATITUDE])[0];
+                })
+                .attr("cy", function(d) {
+                    return projection([d.LONGITUDE, d.LATITUDE])[1];
+                })
+                .attr("r", 2)
+                .on("mouseover", function(d) {
+                  if(d3.select(this).attr("class")=="dot activeDot"){
+                  console.log(""+d.UNIQUE_KEY);
+                  }
+                //})
+               // .append("title")         //Simple tooltip
+                //.text(function(d) {
+                  //   return "Street Name "+d.ON_STREET_NAME+ " key " + d.UNIQUE_KEY;
+                });
+
+  map_svg.selectAll("text")
+         .data(json_fet)
+         .enter()
+         .append("text")
+         .attr("fill", "black")
+         .attr("class", "map-label")
+         .attr("transform", function(d) {
+             var c = path.centroid(d);
+             return "translate("+c[0]+","+c[1]+")"
+         })
+         .attr("text-anchor", "middle")
+         .text(function(d) {
+             return d.properties.BoroName;
+         });
+
+  // make brush for map
+  map_brush = d3.brush()
+                .on("brush end", brushed);
+
+  // map brush
+  map_svg.append("g")
+          .attr("class", "brush")
+          .call(map_brush); 
+}
+
+function initLineChart (data) {
   // Margins
-  var m = {
+  var line_m = {
       top: 10,
       right: 40,
       bottom: 30,
       left: 40
   }
   var chartDiv = document.getElementById("d3_linechart");
-  var w = chartDiv.clientWidth;
-  var h = 200;
+  var time_w = chartDiv.clientWidth;
+  var time_h = 200;
 
-  xScale_time_svg = d3.scaleTime()
-             .domain([minDate, maxDate])
-             .range([0, w]);
-
-  maxValue = d3.max(data.map(function(d) { return d.value; }));
-  console.log(maxValue);
-
-  yScale_time_svg = d3.scaleLinear()
-             .domain([0, maxValue])
-             .range([h, 0]);
-
-  var formatYear = d3.timeFormat("%b-%y");
-
-  xAxis_time_svg = d3.axisBottom()
-            .scale(xScale_time_svg)
-            .tickFormat(formatYear);
-
-  yAxis_time_svg = d3.axisLeft()
-            .scale(yScale_time_svg);  
-
-  line = d3.line()
-           .x(function(d) { return xScale_time_svg(new Date(d.key)); })
-           .y(function(d) { return yScale_time_svg(d.value); });
-
-  // access div element
+  // create time_svg
   time_svg = d3.select("#d3_linechart")
                .append("svg") 
-               .attr("width", w + m.left + m.right)
-               .attr("height", h + m.top + m.bottom*2)
+               .attr("width", time_w + line_m.left + line_m.right)
+               .attr("height", time_h + line_m.top + line_m.bottom*2)
                .append("g")
-               .attr("transform", "translate(" + m.left + "," + m.top + ")");
+               .attr("transform", "translate(" + line_m.left + "," + line_m.top + ")");
 
-  time_svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + h + ")")
-          .call(xAxis_time_svg);
+  // Scales
+  maxValue = d3.max(data.map(function(d) { return d.value; }));
+  xScale_time_svg = d3.scaleTime().domain([minDate, maxDate]).range([0, time_w]);
+  yScale_time_svg = d3.scaleLinear().domain([0, maxValue]).range([time_h, 0]);
 
-  time_svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis_time_svg);
+  // Axes
+  xAxis_time_svg = d3.axisBottom().scale(xScale_time_svg).tickFormat(formatYear);
+  yAxis_time_svg = d3.axisLeft().scale(yScale_time_svg);  
+  time_svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + time_h + ")").call(xAxis_time_svg);
+  time_svg.append("g").attr("class", "y axis").call(yAxis_time_svg);
 
-  // append bins
-  time_svg.append("path")
-          .datum(data)
-          .attr("class", "line")
-          .attr("d", line);
-
-  // append labels
+  // Axis labels
   time_svg.append("text")
-          .attr("x", 0-(h * 0.5))
-          .attr("y", 0-m.left-6)
+          .attr("x", 0-(time_h * 0.5))
+          .attr("y", 0-line_m.left-6)
           .attr("dy", "1em")
           .attr("transform", "rotate(-90)")
           .style("text-anchor", "middle")
@@ -336,46 +229,135 @@ function createLineChart (data) {
 
   time_svg.append("text")
           .attr("transform",
-              "translate(" + (w * 0.5) + " ," +
-              (h + m.bottom) + ")")
+              "translate(" + (time_w * 0.5) + " ," +
+              (time_h + line_m.bottom) + ")")
           .style("text-anchor", "middle")
           .text("Months");
 
+  // Line path
+  line = d3.line()
+           .x(function(d) { return xScale_time_svg(new Date(d.key)); })
+           .y(function(d) { return yScale_time_svg(d.value); });
+  time_svg.append("path")
+          .datum(data)
+          .attr("class", "line")
+          .attr("d", line);
+
   // make brush for timeline
-  brush = d3.brushX()
-                .extent([[0, 0],[w, h - 1]])
-                .on("brush end", brushed);
-
-  // make brush for bar chart
-  bar_brush = d3.brushX()
-                .extent([[0, 0],[w, h - 1]])
-                .on("brush end", from_bar_brushed);
-
-  // make brush for map
-  map_brush = d3.brush()
+  time_brush = d3.brushX()
+                .extent([[0, 0],[time_w, time_h - 1]])
                 .on("brush end", brushed);
 
   // timeline brush
   time_svg.append("g")
           .attr("class", "brush")
-          .call(brush);
+          .call(time_brush);
+
+  time_svg.select('.brush')
+          .call(time_brush);
+
+  // time_svg.select(".brush").call(time_brush.move, [0,0]);
+
+  // change_bar_chart(dataset_all);  
+  // init_time_svg_slider();    
+}
+
+function initBarChart () {
+    var chartDiv = document.getElementById("d3_bar");
+    barW = chartDiv.clientWidth;
+    barH = 200;
+
+    // Margins
+    bar_m = {
+        t: 10,
+        r: 40,
+        b: 30,
+        l: 40
+    }
+
+    //Create scale functions
+    xBarScale = d3.scaleBand()
+                  .domain(hours.map(function(d) { return d; }))
+                  .range([60, barW])
+                  .paddingInner(0.001);
+    console.log(dataset)
+    var ymax = d3.max(dataset);
+
+    yBarScale = d3.scaleLinear()
+                  .domain([0 , ymax])
+                  .range([barH,20]);           
+
+    //Define X axis
+    xAxis_bar = d3.axisBottom(xBarScale);
+            
+    //Define Y axis
+    yAxis_bar = d3.axisLeft(yBarScale)
+              .tickValues(d3.range(0,ymax+1,(ymax < 5) ? 1 : ymax * 0.2));
+
+    bar_svg = d3.select("#d3_bar")
+                .append("svg")
+                .attr("width", barW + bar_m.l + bar_m.r)
+                .attr("height", barH + bar_m.t + bar_m.b);
+
+    bar_svg.selectAll("rect")
+           .data(dataset)
+           .enter()
+           .append("rect")
+           .attr("x", function(d, i) {
+              return xBarScale(i);
+           })
+           .attr("y", function(d) {
+              return yBarScale(d);
+           })
+           .attr("width", xBarScale.bandwidth()-4)
+           .attr("height", function(d) {
+              return barH - yBarScale(d)
+           })
+           .attr("fill", function(d) {
+            return "rgb(0, 0, " + Math.round(yBarScale(d)) + ")";
+           });
+
+    bar_svg.selectAll("text")
+           .data(dataset)
+           .enter()
+           .append("text")
+           .text(function(d) {
+              return d;
+           })
+           .attr("text-anchor", "middle")
+           .attr("x", function(d, i) {
+              return xBarScale(i)+barW/50;
+           })
+           .attr("y", function(d) {
+              return barH - yBarScale(d) - 20;
+           })
+           .attr("font-family", "sans-serif")
+           .attr("font-size", "10px")
+           .attr("fill", "white");
+
+    //Create Axes
+    bar_svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + barH + ")").call(xAxis_bar);
+    bar_svg.append("g").attr("class", "y axis").attr("transform", "translate(60," + 0 + ")").call(yAxis_bar.tickValues(d3.range(0,ymax+1,(ymax < 5) ? 1 : ymax * 0.2)));;    
+
+    // Create Axes labels
+    bar_svg.append("text").style("text-anchor", "middle").text("Hours")
+           .attr("transform", "translate(" + (barW * 0.5 + bar_m.l) + " ," + (barH + bar_m.t + bar_m.b) + ")");    
+    bar_svg.append("text")
+           .attr("transform", "rotate(-90)")
+           .attr("y", 0 - 0)
+           .attr("x", 0 - (barH * 0.5))
+           .attr("dy", "1em")
+           .style("text-anchor", "middle")
+           .text("No of accidents"); 
+
+  // make brush for bar chart
+  bar_brush = d3.brushX()
+                .extent([[0, 0],[barW, barH - 1]])
+                .on("brush end", from_bar_brushed);
 
   bar_svg.append("g")
           .attr("class", "brush")
-          .call(bar_brush);
-
-  // map brush
- /* map_svg.append("g")
-          .attr("class", "brush")
-          .call(map_brush);
-
-  time_svg.select('.brush')
-          .call(brush);
-
-  time_svg.select(".brush").call(brush.move, [0,0]);
-
-  change_bar_chart(dataset_all);  
-  init_time_svg_slider();    */
+          .call(bar_brush);                
 }
 
 function updateLineChartFromDays (noOfDays) {
@@ -392,17 +374,6 @@ function updateLineChartFromDays (noOfDays) {
 }
 
 function updateLineChart (data) {
-  // Margins
-  var m = {
-      top: 10,
-      right: 40,
-      bottom: 30,
-      left: 40
-  }
-
-  var w = 800;
-  var h = 200;
-
   minDate = d3.min(data.map(function(d) { return new Date(d.key); }));
   maxDate = d3.max(data.map(function(d) { return new Date(d.key); }));
 
@@ -470,7 +441,6 @@ function reset_time_svg_slider () {
   time_svg_slider.value(1);
   updateLineChartFromDays(1);
 }
-fromBar = false;
 
 function from_bar_brushed() {
   fromBar = true;
@@ -513,6 +483,7 @@ function updateDots(){
         }
   });
 }
+
 function brushed () {
   sel = d3.event.selection
   
